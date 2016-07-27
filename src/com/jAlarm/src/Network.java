5c3bf9ad96372f2772bf60eac11c8289968168ed
samuelbak <samuele1988@gmail.com>
2016-07-27 15:37:34 +0200

    net

---
+++ src/com/jAlarm/src/Network.java
@@ -0,0 +1,15 @@
+package com.jAlarm.src;
+
+public class Network implements Runnable {
+
+	public Network() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
